9eb965aa95fe031e6e69fd40b2e7a59fdb03d74f
Benjamin Walkenhorst <krylon@posteo.de>
2025-09-30 23:56:12 +0200

    Add modules common, database, engine (the fetcher)

---
+++ src/headlines/engine.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-09-30 19:14:33 krylon>
+#
+# /data/code/python/headlines/src/headlines/engine.py
+# created on 30. 09. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+headlines.engine
+
+(c) 2025 Benjamin Walkenhorst
+
+Engine implements the downloading and processing of RSS feeds.
+"""
+
+
+class Engine:
+    pass
+
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
