6c38aa7a166e61f87056796562b1419c180e650d
Benjamin Walkenhorst <krylon@posteo.de>
2025-10-09 23:07:59 +0200

    engine - start implementing

--- engine.py
+++ engine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-10-04 15:09:42 krylon>
+# Time-stamp: <2025-10-09 23:06:55 krylon>
 #
 # /data/code/python/headlines/src/headlines/engine.py
 # created on 30. 09. 2025
@@ -19,8 +19,80 @@ Engine implements the downloading and processing of RSS feeds.
 """
 
 
-class Engine:  # pylint: disable-msg=R0903
-    """Engine implements the fetching an processing of RSS feeds."""
+import logging
+from datetime import timedelta
+from queue import SimpleQueue
+from threading import Lock
+from typing import Union
+
+from headlines import common
+from headlines.database import Database
+
+
+class Engine:
+    """Fetcher downloads RSS feeds."""
+
+    __slots__ = [
+        "log",
+        "db",
+        "interval",
+        "lock",
+        "_active",
+        "feedq",
+        "itemq",
+    ]
+
+    log: logging.Logger
+    db: Database
+    interval: timedelta
+    lock: Lock
+    _active: bool
+    feedq: SimpleQueue
+    itemq: SimpleQueue
+
+    def __init__(self, interval: Union[int, float, timedelta]) -> None:
+        self.log = common.get_logger("engine")
+        self.db = Database()
+        self.lock = Lock()
+        self._active = False
+        self.feedq = SimpleQueue()
+        self.itemq = SimpleQueue()
+        match interval:
+            case int(x):
+                self.interval = timedelta(seconds=x)
+            case float(x):
+                self.interval = timedelta(seconds=x)
+            case x if isinstance(x, timedelta):
+                self.interval = x
+            case _:
+                name = interval.__class__.__name__
+                msg = f"Interval must be a number (of seconds) or a timedelta, not a {name}"
+                raise ValueError(msg)
+
+        self.log.debug("Engine will check for news every %s seconds.",
+                       self.interval.seconds)
+
+    @property
+    def active(self) -> bool:
+        """Return the Engine's active flag."""
+        with self.lock:
+            return self._active
+
+    @active.setter
+    def active(self, value: bool) -> None:
+        """Set the Engine's active flag."""
+        with self.lock:
+            self._active = value
+
+    def start(self) -> None:
+        """Begin to periodically check the datbase for Feeds due for a refresh."""
+        self.log.debug("Engine is starting.")
+        self.active = True
+
+    def _loop(self) -> None:
+        """Perform the outermost main loop."""
+        while self.active:
+            pass
 
 
 # Local Variables: #
