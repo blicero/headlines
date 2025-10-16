43bd2066c4bc8f6f93b3c2412b3e6c8291273e81
Benjamin Walkenhorst <krylon@posteo.de>
2025-10-16 16:58:22 +0200

    add module classy for guessing Ratings on Items

---
+++ classy.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-10-16 16:31:27 krylon>
+#
+# /data/code/python/headlines/classy.py
+# created on 15. 10. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+headlines.classy
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+
+import logging
+from dataclasses import dataclass, field
+from threading import Lock
+from typing import Final
+
+from bs4 import BeautifulSoup
+from simplebayes import SimpleBayes
+
+from headlines import common
+from headlines.model import Item, Rating
+
+
+@dataclass(kw_only=True, slots=True)
+class Karl:
+    """Karl classifies Items as boring or interesting.
+
+    Get it? Karl, because Karl Marx and class struggle? I'll see myself out.
+    """
+
+    log: logging.Logger = field(default_factory=lambda: common.get_logger("Karl"))
+    lock: Lock = field(default_factory=Lock)
+    bayes: SimpleBayes = field(default_factory=lambda: SimpleBayes(cache_path=common.path.spool))
+
+    def __post_init(self) -> None:  # pylint: disable-msg=W0238
+        if not self.bayes.cache_train():
+            self.log.error("Failed to train classifier")
+
+    def item_text(self, item: Item) -> str:
+        """Return the plain text from an Item."""
+        # TODO Maybe add some caching later on.
+        raw: Final[str] = item.headline + " " + item.body
+        soup = BeautifulSoup(raw, "html.parser")
+        plain: Final[str] = soup.get_text()
+        return plain.lower()
+
+    def classify(self, item: Item) -> Rating:
+        """Classify an Item based on trained data."""
+        with self.lock:
+            plain = self.item_text(item)
+            rating: Final[Rating] = Rating.from_str(self.bayes.classify(plain))
+            item.cache_rating(rating)
+            return rating
+
+    def rate(self, item: Item, rating: Rating) -> None:
+        """Add an Item and its Rating to the training data."""
+        with self.lock:
+            try:
+                plain = self.item_text(item)
+                self.bayes.train(rating.name, plain)
+            except Exception as err:  # pylint: disable-msg=W0718
+                cname: Final[str] = err.__class__.__name__
+                self.log.error("%s trying to train on Item %d (%s): %s",
+                               cname,
+                               item.item_id,
+                               item.headline,
+                               err)
+            else:
+                self.bayes.cache_persist()
+
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
